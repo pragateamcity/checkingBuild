95dab039c9d0e1e38b481904b324095171b9717e
pragateamcity <115547117+pragateamcity@users.noreply.github.com>
2022-10-12 16:21:20 +0530

    Add files via upload

---
+++ DemoMVC/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "<h1>Hello C# Corner. This is My First MVC APP</h1>";
+        }
+
+    }
+}
